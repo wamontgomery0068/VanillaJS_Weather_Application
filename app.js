aab7f8a7b4053dade9e02ef81e1e58e60252b365
Your Name <wamontgomery0068@gmail.com>
2019-03-28 12:02:15 -0600

    click on the degree it changes from F to C

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ window.addEventListener('load', () => {
     let locationTimezone = document.querySelector('.location_timezone');
     let temperatureDegree = document.querySelector('.temperature_degree');
     let temperatureDescription = document.querySelector('.temperature_description');
+    let temperatureSection = document.querySelector('.temperature');
+    const temperatureSpan = document.querySelector('.temperature span');
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
@@ -33,16 +35,44 @@ window.addEventListener('load', () => {
             fetch(api).then(response => {
                 return response.json();
             }).then(data => {
+
                 // console.log(data);
-                const { temperature, summary } = data.currently;
+
+                const { temperature, summary, icon } = data.currently;
+
                 // SET DOM Elements from the API
+
                 locationTimezone.textContent = data.timezone;
                 temperatureDegree.textContent = temperature;
                 temperatureDescription.textContent = summary;
 
+                // Formula For Celsius
+
+                let celsius = (temperature - 32 ) * (5/9);
+
+                // Set Icon
+
+                setIcons(icon, document.querySelector('.icon'));
+
+                // Change temperature to Celsius/Farenheit
+
+                temperatureSection.addEventListener('click', () => {
+                    if(temperatureSpan.textContent === "F"){
+                        temperatureSpan.textContent = "C";
+                        temperatureDegree.textContent = Math.floor(celsius);
+                    } else {
+                        temperatureSpan.textContent = "F";
+                    }
+                })
+
             });
-
         });
+    }
 
+    function setIcons(icon, iconID){
+        const skycons = new Skycons({color:"white"});
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
     }
 });
